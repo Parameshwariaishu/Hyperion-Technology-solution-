2974e7063a270598dc85339d9e06ba59ca148914
Parameshwariaishu <aishwariyaparameshwari@gmail.com>
2024-12-02 20:40:39 +0530

    Hyperion

---
+++ script.js
@@ -0,0 +1,54 @@
+// var data= document.getElementsByClassName(".listpara")
+// function myFunction(){
+// data.style="display:block"
+// }
+
+function toggleParagraph() {
+    const paragraph = document.getElementById('listpara');
+    paragraph.style.display = paragraph.style.display === 'none' ? 'block' : 'none';
+  }
+  function toggleParagraph2() {
+    const paragraph = document.getElementById('listpara2');
+    paragraph.style.display = paragraph.style.display === 'none' ? 'block' : 'none';
+  }
+  function toggleParagraph3() {
+    const paragraph = document.getElementById('listpara3');
+    paragraph.style.display = paragraph.style.display === 'none' ? 'block' : 'none';
+  }
+  function toggleParagraph4() {
+    const paragraph = document.getElementById('listpara4');
+    paragraph.style.display = paragraph.style.display === 'none' ? 'block' : 'none';
+  }
+  function toggleParagraph5() {
+    const paragraph = document.getElementById('listpara5');
+    paragraph.style.display = paragraph.style.display === 'none' ? 'block' : 'none';
+  }
+  function toggleParagraph6() {
+    const paragraph = document.getElementById('listpara6');
+    paragraph.style.display = paragraph.style.display === 'none' ? 'block' : 'none';
+  }
+  function toggleParagraph7() {
+    const paragraph = document.getElementById('listpara7');
+    paragraph.style.display = paragraph.style.display === 'none' ? 'block' : 'none';
+  }
+  function toggleParagraph8() {
+    const paragraph = document.getElementById('listpara8');
+    paragraph.style.display = paragraph.style.display === 'none' ? 'block' : 'none';
+  }
+  function toggleParagraph9() {
+    const paragraph = document.getElementById('listpara9');
+    paragraph.style.display = paragraph.style.display === 'none' ? 'block' : 'none';
+  }
+  function toggleParagraph10() {
+    const paragraph = document.getElementById('listpara10');
+    paragraph.style.display = paragraph.style.display === 'none' ? 'block' : 'none';
+  }
+  function toggleParagraph11() {
+    const paragraph = document.getElementById('listpara11');
+    paragraph.style.display = paragraph.style.display === 'none' ? 'block' : 'none';
+  }
+  function toggleParagraph12() {
+    const paragraph = document.getElementById('listpara12');
+    paragraph.style.display = paragraph.style.display === 'none' ? 'block' : 'none';
+  }
+
